4064e45e00c1fd3ac7570695e4f7534164679efa
Blake Sheffield <blakeshef1@gmail.com>
2025-07-10 01:35:04 -0400

    Implemented groups and refactored main

--- main.py
+++ main.py
@@ -12,18 +12,24 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     player = Player(
         SCREEN_WIDTH / 2,
         SCREEN_HEIGHT / 2
     )
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(pygame.Color(0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        for item in updateable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
